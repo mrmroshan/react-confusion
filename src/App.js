7ef1f4e9053976200bc10537d6b3af5e207cd93f
Roshan Ruzaik <mrmroshan@yahoo.com>
2020-09-21 07:38:39 +0300

    Menu componentn constructed with Media reactstrap object

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
-import {Navbar,NavbarBrand} from 'reactstrap';
+import {Navbar,NavbarBrand, Media} from 'reactstrap';
 import './App.css';
+
+import Menu from './components/MenuComponent';
 
 class App extends Component {
   render (){
     return (
-      <div className="App">
+      <div>
         <Navbar dark color="primary">
         <div className="container">
           <NavbarBrand href="/">Restorante Con Fusion</NavbarBrand>
         </div>
         </Navbar>
+        <Menu />
       </div>
     );
   }
