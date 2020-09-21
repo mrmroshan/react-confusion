f685fe589e6e11d4c8c4db09b11bc363a214cc33
Roshan Ruzaik <mrmroshan@yahoo.com>
2020-09-21 07:13:44 +0300

    Basic navbar changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React, { Component } from 'react';
-import './App.css';
-
 import {Navbar,NavbarBrand} from 'reactstrap';
+import './App.css';
 
 class App extends Component {
   render (){
     return (
       <div className="App">
-        <NavbarBrand dark color="primary">
+        <Navbar dark color="primary">
         <div className="container">
           <NavbarBrand href="/">Restorante Con Fusion</NavbarBrand>
         </div>
-        </NavbarBrand>
+        </Navbar>
       </div>
     );
   }
