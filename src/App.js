b2f054f47773ad6f82a54c69b91e2fc198634163
Roshan Ruzaik <mrmroshan@yahoo.com>
2020-09-22 06:48:18 +0300

    Exampe of how to lift the state to a parent component and how to pass those data to child component. Seperation of shared data to a common place

--- src/App.js
+++ src/App.js
@@ -3,8 +3,19 @@ import {Navbar,NavbarBrand, Media} from 'reactstrap';
 import './App.css';
 
 import Menu from './components/MenuComponent';
+import DISHES from './shared/dishes';
+
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      dishes: DISHES
+    }
+  }
+
+
   render (){
     return (
       <div>
@@ -13,7 +24,7 @@ class App extends Component {
           <NavbarBrand href="/">Restorante Con Fusion</NavbarBrand>
         </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes}/>
       </div>
     );
   }
